212b9d5a8a9a566cdae226b066e8e420609829ac
Pitrow <elultim0c0rle0ne@hotmail.com>
2017-09-14 15:57:14 +0200

    Corregidos nombres y estructura de los ejercicios.

---
+++ ejercicios/Pitrow/practica-01/ej-01.php
@@ -0,0 +1,7 @@
+<?php
+
+$name = "Pepe";
+$age = 1000;
+$add = "Senda de la Amargura Nº 666";
+
+echo "Soy ".$name.", tengo ".$age." años y vivo en ".$add;
---
+++ ejercicios/Pitrow/practica-01/ej-03.php
@@ -0,0 +1,6 @@
+<?php
+
+$asistentes = mt_rand(0, 10000);
+$invitados = mt_rand(20, 500);
+
+echo "Han venido ".$asistentes." personas, invité a ".$invitados."\n";
